2061e623412159c14d65ef2cae6e4a133062623f
superizer <superizer.tone@gmail.com>
2017-10-18 15:44:46 +0700

    Optimize numberquesser

--- numberguesser.py
+++ numberguesser.py
@@ -1,11 +1,7 @@
 import random as r
 num = r.randint(1,100)
-guessed = False
 guess = input("Guess my number (1-100):")
-while not guessed:
-    if(int(guess) == num):
-        guessed = True
-    else:
-        print("lower" if int(guess)>num else "higher");
-        guess = input("Next guess:")
+while int(guess) != num:
+	print("lower" if int(guess)>num else "higher");
+	guess = input("Next guess:")
 print("you guessed correctly!")
